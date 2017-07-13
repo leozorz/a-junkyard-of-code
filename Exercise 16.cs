804876cae94ba1b685d444ebab2a7ea6e051ef5a
leozorz <the.cake.is.a.lie_@hotmail.com>
2017-07-13 16:58:07 +0100

    Update Exercise 16.cs

--- Exercise 16.cs
+++ Exercise 16.cs
@@ -4,26 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp5
+namespace ConsoleApp7
 {
     class Program
     {
-        public static string FrontBack(string str)
-        {
-            int len = str.Length;
-            return str[len - 1] + str.Substring(1, len - 2) + str[0];
-        }
-
         static void Main(string[] args)
         {
-            Console.WriteLine("Choose your string.");
-            string s1 = Console.ReadLine();
-            
-            string s2 = FrontBack(s1);
-            
-            Console.WriteLine("Here you go: " + s2);
-            Console.WriteLine("Press any key to quit...");
-            Console.ReadLine();          
+
+            string firstString = "w3resource";
+
+            // string secString = "Python";
+
+            string finalString = firstString.Substring(firstString.Length - 1) +firstString.Substring(1, firstString.Length - 2) + firstString.Substring(0, 1);
+
+            Console.Write(finalString + "\n");
+
+            Console.ReadKey();
 
         }
     }
