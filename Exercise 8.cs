64df94b39f91e9cec37f38c19f2ae83e73df160a
leozorz <the.cake.is.a.lie_@hotmail.com>
2017-07-13 16:42:14 +0100

    Update Exercise 8.cs

--- Exercise 8.cs
+++ Exercise 8.cs
@@ -10,21 +10,22 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-       
+
+            double sum;
+
             Console.WriteLine("Please enter a number");
             double a = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("{0} x 0 = {1}", a, a*0);
-            Console.WriteLine("{0} x 1 = {1}", a, a*1);
-            Console.WriteLine("{0} x 2 = {1}", a, a * 2);
-            Console.WriteLine("{0} x 3 = {1}", a, a * 3);
-            Console.WriteLine("{0} x 4 = {1}", a, a * 4);
-            Console.WriteLine("{0} x 5 = {1}", a, a * 5);
-            Console.WriteLine("{0} x 6 = {1}", a, a * 6);
-            Console.WriteLine("{0} x 7 = {1}", a, a * 7);
-            Console.WriteLine("{0} x 8 = {1}", a, a * 8);
-            Console.WriteLine("{0} x 9 = {1}", a, a * 9);
-            Console.WriteLine("{0} x 10 = {1}", a, a * 10);
+
+            for (int i = 0; i < 11; i++)
+
+            {
+
+                sum = a * i;
+
+            Console.WriteLine("{0} x {1} = {2}", a, i, sum);
+
+            }
 
             Console.WriteLine("Press any key...");
             Console.ReadLine();
