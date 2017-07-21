1b7aafa1f77895a31e1113e0a7c2046b8ca7afe1
leozorz <the.cake.is.a.lie_@hotmail.com>
2017-07-21 17:57:59 +0100

    Create Exercise 24.cs

---
+++ Exercise 24.cs
@@ -0,0 +1,32 @@
+using System;
+public class Exercise24
+{
+    public static void Main()
+    {
+        Console.WriteLine("Find the biggest word in the line:\n");
+
+        string line = "Write a C# Sharp Program to display the following pattern using the alphabet.";
+
+        Console.WriteLine(line + "\n");
+
+        string[] words = line.Split(new[] { " " }, StringSplitOptions.None);
+
+        string word = "";
+
+        int control = 0;
+
+        foreach (String s in words)
+        {
+            if (s.Length > control)
+            {
+                word = s;
+                control = s.Length;
+            }
+        }
+
+        Console.WriteLine(word);
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
