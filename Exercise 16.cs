8ad4404a7ae1e1453022286a8d3b44d1179b95e9
leozorz <the.cake.is.a.lie_@hotmail.com>
2017-08-24 11:52:22 +0100

    Update Exercise 16.cs

--- Exercise 16.cs
+++ Exercise 16.cs
@@ -4,23 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp7
+namespace ConsoleApp14
 {
     class Program
     {
         static void Main(string[] args)
         {
-
             string firstString = "w3resource";
 
-            // string secString = "Python";
+            string secString = "Python";
 
-            string finalString = firstString.Substring(firstString.Length - 1) +firstString.Substring(1, firstString.Length - 2) + firstString.Substring(0, 1);
+            string finalString = firstString.Substring(firstString.Length - 1) + firstString.Substring(1, firstString.Length - 2) + firstString.Substring(0, 1);
+
+            string finalsecstring = secString.Substring(secString.Length - 1) + secString.Substring(1, secString.Length - 2) + secString.Substring(0, 1);
 
             Console.Write(finalString + "\n");
 
-            Console.ReadKey();
+            Console.WriteLine(finalsecstring + "\n");
 
+            Console.ReadKey();
         }
     }
 }
